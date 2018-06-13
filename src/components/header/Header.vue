d7f63cd4eec0cddbfae2cf80651effdfa8785e36
xiaoxiaocdx <18738126862@163.com>
2018-06-13 21:13:48 +0800

    福利页面

---
+++ src/components/header/Header.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <div class="page-cover" v-show="menuShow" @click="isShow"></div>
+  <header class="header" :class="{'show':menuShow}" @click="isShow">
+    <header class="bar bar-nav">
+      <div class="pull-left">
+        <span class="iconfont icon-fenlei"></span>
+      </div>
+      <div class="title">{{headerTitle}}</div>
+      <div class="pull-right">
+        <span class="iconfont icon-sousou_souusou"></span>
+      </div>
+    </header>
+  </header>
+  <v-menu :show="menuShow"></v-menu>
+</div>
+</template>
+<script type="text/ecmascript-6">
+  import {mapState} from 'store'
+  export default
+  {
+    name:'v-header',
+    components:{
+      vMenu
+    },
+    data(){
+      return{
+        show:false
+      };
+    },
+    computed:{
+      ...mapState([
+        'headerTitle','menuShow'
+      ])
+    },
+    methods:{
+      isShow(){
+        this.$store.commit('UPDATE_MENUSHOW');
+      },
+      hideDetail(){
+        this.detailShow=false;
+      }
+    }
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "Header.styl";
+</style>
